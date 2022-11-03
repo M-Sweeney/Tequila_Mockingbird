5227d93c42561da9b7238937c14ef5a2a6ab9423
Matt <sweeney.matt93@gmail.com>
2022-11-03 16:21:43 -0500

    started the functionality of the featured drink on the home page

--- tequila_mockingbird/src/components/Home.jsx
+++ tequila_mockingbird/src/components/Home.jsx
@@ -4,16 +4,15 @@ import axios from 'axios'
 export default function Drinks () {
 
   //create state for data
-  const [drinks, setDrinks] = useState({})
+  const [drinks, setDrinks] = useState([])
 
   //call axios function
 useEffect(()=>{
   const getData = async () =>{
-  const response = await axios.get('www.thecocktaildb.com/api/json/v1/1/random.php')
+  const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
   // console.log(response.data.drinks)
   // set state of our data
   setDrinks(response.data.drinks)
-console.log(response)
   }
 
   getData()
@@ -21,27 +20,28 @@ console.log(response)
 }, [])
 
   //see the data
-// console.log(drinks)
+console.log(drinks)
 
 
   // make a guard operator 
 if(!drinks) {
-  return <h2>Loading Drinks</h2>
+  return <h2>Loading Featured Drink</h2>
 }else{
   return(
     <div className='container'>
     <div className="title">
       <h1>Featured Drink!</h1>
     </div>
+
     <div className='drinkDetails'>
       {
-      <div key={drink.strDrink}
-      className='card'>
-      <img className="preview" src={`${drink.strDrinkThumb}`}  />
-      <div className="previewText">
-      <h2>{drink.strDrink}</h2>
-      <h3>{drink.strAlcoholic}</h3>
-      <h3>{drink.strCategory}</h3>
+      <div key={drinks.strDrink}
+      className='detailsCard'>
+      <img className="detailsImg" src={`${drinks[0].strDrinkThumb}`}  />
+      <div className="detailText">
+      <h2 className="cardTitle">{drinks[0].strDrink}</h2>
+      <h3>{drinks[0].strAlcoholic}</h3>
+      <h3>{drinks[0].strCategory}</h3>
       </div>
       </div>
       }
