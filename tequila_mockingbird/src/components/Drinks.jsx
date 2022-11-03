d717734bba88bb40dd6a8c46b86d9d4adad92251
Matt <sweeney.matt93@gmail.com>
2022-11-03 11:31:29 -0500

    axious is working

--- tequila_mockingbird/src/components/Drinks.jsx
+++ tequila_mockingbird/src/components/Drinks.jsx
@@ -2,9 +2,35 @@ import { useState, useEffect } from "react"
 import axios from 'axios'
 
 export default function Drinks () {
+
+  //create state for data
+  const [drinks, setDrinks] = useState({})
+
+  //call axios function
+useEffect(()=>{
+  const getData = async () =>{
+  const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+  console.log(response.data)
+  // set state of our data
+  setDrinks(response.data)
+
+  }
+
+  getData()
+
+}, [])
+
+  //see the data
+console.log(drinks)
+
+  //make a guard operator 
+
+if(!drinks) {
+  return <h2>Loading Drinks</h2>
+}else{
   return(
     <div>
-      <h2>Drinks</h2>
+      <h2>drinks: </h2>
     </div>
-  )
+  )}
 }
