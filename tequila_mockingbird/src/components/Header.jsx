70681f0fc5e90de24292b09070785c6f1b7d76e5
Matt <sweeney.matt93@gmail.com>
2022-11-08 16:17:01 -0600

    styling updates

--- tequila_mockingbird/src/components/Header.jsx
+++ tequila_mockingbird/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import Nav from "./Nav"
 import Banner from '../assets/Banner.jpg'
+import Banner2 from '../assets/banner2.png'
+
 
 
 export default function Header ({search, setSearch}) {
@@ -7,7 +9,7 @@ export default function Header ({search, setSearch}) {
 
   return(
     <div>
-      <img src={Banner} className="banner"/>
+      <img src={Banner2} className="banner"/>
       <h1 className="headerTitle">Tequila Mockingbird</h1>
       <Nav search={search} setSearch={setSearch}/>
     </div>
